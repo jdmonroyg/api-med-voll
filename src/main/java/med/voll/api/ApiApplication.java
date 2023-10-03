0d3816894610340a91aaf38bda415ffb351bb0b4
Jesus Monroy <jdmonroyg@unal.edu.co>
2023-10-03 14:23:53 -0500

    Se agrego un archivo local *.env para utilizar las variables de entorno en el proyecto y se realiazon un cambio en la ejecucion de la aplicacion para que genere un war

--- src/main/java/med/voll/api/ApiApplication.java
+++ src/main/java/med/voll/api/ApiApplication.java
@@ -2,12 +2,26 @@ package med.voll.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+//@SpringBootApplication
+//public class ApiApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(ApiApplication.class, args);
+//	}
+//
+//}
+
+// Configuracion para generar el war
 @SpringBootApplication
-public class ApiApplication {
-
+public class ApiApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApiApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-
 }
